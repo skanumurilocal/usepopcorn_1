c1c8109612a52ea165c11277cec1770e9be22518
CodeSandbox <support@codesandbox.io>
2023-11-08 00:05:13 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,5 +10,5 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
